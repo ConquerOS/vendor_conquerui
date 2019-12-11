6c5c3faa557826b1701b3c5c2178e18309ed9714
Dyneteve <sound0020@gmail.com>
2019-12-11 14:51:52 +0000

    ConquerUI: Define themepicker_overlayable_package
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

---
+++ overlay/common/frameworks/base/packages/SystemUI/res/values/config.mk
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string name="launcher_overlayable_package">com.google.android.apps.nexuslauncher</string>
+    <string name="themepicker_overlayable_package">com.google.android.apps.wallpaper</string>
+</resources>
