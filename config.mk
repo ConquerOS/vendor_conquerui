b5474957d1c27662207644f2fc2289d776fa55b6
HeroBuxx <herobuxx@gmail.com>
2019-12-20 15:13:50 +0000

    ConquerUI: FUCK ALL LAUNCHERS
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- config.mk
+++ config.mk
@@ -52,11 +52,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay/common/
 
-# Include OnePlus Launcher
-$(warning "ConquerUI: OnePlus Launcher is set as default Launcher")
-PRODUCT_PACKAGES += \
-    OPLauncher
-
 # Pixel Customization
 PRODUCT_PACKAGES += \
     ThemePicker
